cce9f15b5b06683d45141687a6d683afb5e6148f
priyanshi <priyanshisaxena628@gmail.com>
2022-04-14 17:03:11 +0530

    connectedLoginHome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './salesperson/components/home/home.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path:"signup",
     component: SignupComponent
+  },
+  {
+    path:"home",
+    component: HomeComponent
   }
 ];
 
